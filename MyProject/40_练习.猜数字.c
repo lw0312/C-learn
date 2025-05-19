fba8448e4ead20fd3b037e5ec6fa2efdee37c2ca
罗 伟 <2314446096@qq.com>
2025-05-19 18:33:21 +0800

    f

--- MyProject/40_练习.猜数字.c
+++ MyProject/40_练习.猜数字.c
@@ -1,28 +1,28 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-
-int main() {
-	srand(time(NULL));
-
-	int num = rand() % 100 + 1;
-
-	while (1) {
-		int putNum;
-		printf("请输入数字:");
-		scanf("%d", &putNum);
-
-		if (putNum == num) {
-			printf("恭喜你,猜对了,%d",num);
-			break;
-		}
-		else if(putNum > num) {
-			printf("猜错了,大了\n");
-		}
-		else if(putNum < num){
-			printf("猜错了,小了\n");
-		}
-	}
-
-	return 0;
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<time.h>
+//
+//int main() {
+//	srand(time(NULL));
+//
+//	int num = rand() % 100 + 1;
+//
+//	while (1) {
+//		int putNum;
+//		printf("请输入数字:");
+//		scanf("%d", &putNum);
+//
+//		if (putNum == num) {
+//			printf("恭喜你,猜对了,%d",num);
+//			break;
+//		}
+//		else if(putNum > num) {
+//			printf("猜错了,大了\n");
+//		}
+//		else if(putNum < num){
+//			printf("猜错了,小了\n");
+//		}
+//	}
+//
+//	return 0;
+//}
